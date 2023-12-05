c6bdfc7481548f7453bd3c73a20172cecbba5e5b
kalenduhs <kalenduhs@gmail.com>
2023-12-04 20:24:11 -0600

    Attempted Studio

---
+++ control-flow-and-collections/studio/counting-characters/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String phrase = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        char[] charactersInString = phrase.toCharArray();
+
+        HashMap<Character, Integer> counted = new HashMap<>();
+        /*for (char i : charactersInString) {
+            counted.put(i, 0);
+        }*/
+
+        }
+    }
+}
