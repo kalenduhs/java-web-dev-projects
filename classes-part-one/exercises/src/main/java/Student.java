6c3834677bd1d2e40cb8a6147919409d8262eb38
kalenduhs <kalenduhs@gmail.com>
2023-12-06 16:54:04 -0600

    Completed Classes part 1 exercises

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -7,4 +7,44 @@ public class Student {
         // Drop your getters and setters below for the Student class.
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
+        public Student(String name, int studentId, int numberOfCredits, double gpa) {
+                this.name = name;
+                this.studentId = studentId;
+                this.numberOfCredits = numberOfCredits;
+                this.gpa = gpa;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String aName) {
+                name = aName;
+        }
+
+        public int getStudentId() {
+                return studentId;
+        }
+
+        public void setStudentId(int aStudentID) {
+                studentId = aStudentID;
+        }
+
+        public int getNumberOfCredits() {
+                return numberOfCredits;
+        }
+
+        private void setNumberOfCredits(int aNumberOfCredits) {
+                numberOfCredits = aNumberOfCredits;
+        }
+
+        public double getGpa() {
+                return gpa;
+        }
+
+        public void setGpa(double aGpa) {
+                gpa = aGpa;
+        }
+
+
 }
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,9 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+
+        Student student = new Student("Kalen", 69420, 1, 4.0);
+
+        System.out.println(student.getName());
     }
 }
