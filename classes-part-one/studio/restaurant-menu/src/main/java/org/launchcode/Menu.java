8c5c43301c4fc14ccfc20e7338465eab6c21bbbc
kalenduhs <kalenduhs@gmail.com>
2023-12-07 19:54:06 -0600

    Completed Studio

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,16 @@
+package org.launchcode;
+
+import java.util.ArrayList;
+
+public class Menu {
+
+    private ArrayList<MenuItems> menu;
+
+    private boolean isNew;
+
+    //Construct menu items here? Possibly in main and add to Menu class
+
+    //Constructor for Menu (menu, isNew)
+
+    //Getters and Setters
+}
---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItems.java
@@ -0,0 +1,50 @@
+package org.launchcode;
+
+public class MenuItems {
+    private double price;
+
+    private String description;
+
+    private String category;
+
+    private boolean isNew;
+
+    public MenuItems(double price, String description, String category, boolean isNew) {
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
+}
