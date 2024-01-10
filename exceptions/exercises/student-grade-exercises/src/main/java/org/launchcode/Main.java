3296230e53e49be625284efd423f5f5af2c61963
kalenduhs <kalenduhs@gmail.com>
2024-01-10 16:54:49 -0600

    Completed exceptions exercises

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,16 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        try {
+            System.out.println(Divide(5,0));
+        } catch (ArithmeticException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,15 +19,36 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for(Map.Entry<String, String> entry : studentFiles.entrySet()) {
+            try {
+                System.out.println(entry.getKey() + " scored a " + CheckFileExtension(entry.getValue()));
+            } catch (RuntimeException e) {
+                System.out.println(entry.getKey() + " " + e.getLocalizedMessage());
+            }
+        }
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
-        // Write code here!
+        if (y == 0) {
+            throw new ArithmeticException("Don't divide by 0");
+            }
+        return  x / y;
     }
+
+
 
     public static int CheckFileExtension(String fileName)
     {
         // Write code here!
+        //String correct = ".java";
+        if (fileName.contains(".java")) {
+            return 1;
+        } else if (fileName.isEmpty()) {
+            throw new RuntimeException("Please input a file name");
+        } else {
+            return 0;
+        }
     }
-}
+
+    }
